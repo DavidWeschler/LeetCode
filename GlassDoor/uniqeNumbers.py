8b6408a3d49047628b506020c81d8d19ff5fe0f0
David Weschler <davidweschler8@gmail.com>
2024-09-28 23:56:24 +0300

    uniqe nums task from leetcode

---
+++ GlassDoor/uniqeNumbers.py
@@ -0,0 +1,51 @@
+'''
+Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+Example 1:
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+Example 2:
+
+Input: arr = [1,2]
+Output: false
+Example 3:
+
+Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+Output: true
+ 
+
+Constraints:
+
+1 <= arr.length <= 1000
+-1000 <= arr[i] <= 1000
+'''
+
+from typing import List
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        if not arr:
+            return True
+        arr.sort()
+        occur = {}
+        curr = 0
+        currNum = arr[0]
+        for i in range(len(arr)):
+            if arr[i] != currNum:
+                if curr in occur:
+                    return False
+                else:
+                    occur[curr] = currNum
+                    curr = 0
+                    currNum = arr[i]
+            curr += 1
+        return True
+
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.uniqueOccurrences([1,2,2,1,1,3]))
+    print(sol.uniqueOccurrences([1,2]))
+    print(sol.uniqueOccurrences([-3,0,1,-3,1,1,1,-3,10,0]))
